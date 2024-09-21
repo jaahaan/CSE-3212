7ec89eff7fd5e6c196e95903c3d9ad3ec28913b1
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-09-21 18:01:30 +0600

    submission: assignment_1

---
+++ README.md
@@ -0,0 +1,5 @@
+#### CSE-3212: Smart Application Development
+
+Assignment logs related to the course CSE-3212: Smart Application Development
+
+- [ ] [Assignment 1]('https://github.com/irfanshadikrishad/CSE-3212/tree/assignment_1')
---
+++ app/src/main/java/com/example/est/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.est
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonClick = findViewById<Button>(R.id.button)
+
+        buttonClick.setOnClickListener {
+            Toast.makeText(this, "Thanks for clicking!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
